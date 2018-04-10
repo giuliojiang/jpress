8f7b84d41f0a34364e39257a9ac660f00943bc83
giuliojiang <giuliojiang@gmail.com>
2018-04-10 17:59:09 +0200

    Implement write/upload button

--- client/upload.controller.js
+++
@@ -1,7 +0,0 @@
-mainApp.controller("upload", function($scope, binupload) {
-
-    $scope.select_file = function() {
-        binupload.upload_start_select_file();
-    };
-
-});
--- client/write/upload/upload.controller.js
+++ client/write/upload/upload.controller.js
@@ -1,3 +1,28 @@
-mainApp.controller("write.upload", function($scope) {
+mainApp.controller("write.upload", function($scope, binupload) {
+
+    var self = this;
+    var priv = {};
+
+    priv.init = function() {
+        binupload.register_on_upload_complete(function(file_url, file_name) {
+            console.info("write/upload: upload complete for ["+ file_url +"] ["+ file_name +"]");
+        });
+    }
+
+    $scope.select_file = function() {
+        binupload.upload_start_select_file();
+    };
+
+    // init ===================================================================
+
+    self.$onInit = function() {
+        priv.init();
+    }
+
+    // destroy ================================================================
+
+    self.$onDestroy = function() {
+        binupload.register_on_upload_complete(null);
+    }
 
 });
