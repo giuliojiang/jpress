b8f6d82ec817ff54bc959b956c4c8e2e8b01de81
BuildTools <unconfigured@null.spigotmc.org>
2018-08-28 18:28:11 +0100

    Add scripts to perform useful MongoDB actions

---
+++ script/mongoCreateCollection.js
@@ -0,0 +1,28 @@
+var MongoClient = require('mongodb').MongoClient;
+var util = require("util");
+
+var uri = "";
+
+var main = async function() {
+
+    // Connect
+    var mongoClientConnectFunc = util.promisify(MongoClient.connect);
+    var client = await mongoClientConnectFunc(uri);
+
+    // Get the db
+    var db = client.db("test");
+    console.info("Got a db? " + !!db);
+
+    // Create the collection
+    db.createCollection("jpress", function(err, res) {
+        if (err) {
+            console.error(err);
+            return;
+        } else {
+            console.info("Collection created");
+        }
+    });
+
+}
+
+main();
